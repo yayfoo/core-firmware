ac5af85cb39ac67f3bdd00ad75fe38f281941b1a
Zach Supalla <zsupalla@gmail.com>
2013-04-10 01:22:47 +0800

    Pin mappings and basic GPIO. Untested.

--- inc/arduinito.h
+++ inc/arduinito.h
@@ -2,12 +2,100 @@
  * arduinito.h
  *
  *  Created on: Apr 3, 2013
- *      Author: zac
+ *      Author: Zach Supalla
  */
 
 #ifndef ARDUINITO_H_
 #define ARDUINITO_H_
 
-void digitalWrite(char pin, char val);
+/*
+ * Pin mapping. Borrowed from Wiring
+ */
+
+#define TOTAL_PINS 21
+#define TOTAL_ANALOG_PINS 8
+#define FIRST_ANALOG_PIN 10
+
+#define D0 0
+#define D1 1
+#define D2 2
+#define D3 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+
+#define LED1 8
+#define LED2 9
+
+#define A0 10
+#define A1 11
+#define A2 12
+#define A3 13
+#define A4 14
+#define A5 15
+#define A6 16
+#define A7 17
+
+#define RX 18
+#define TX 19
+
+#define BTN 20
+
+// Timer pins
+
+#define TIMER2_CH1 10
+#define TIMER2_CH2 11
+#define TIMER2_CH3 18
+#define TIMER2_CH4 19
+
+#define TIMER3_CH1 14
+#define TIMER3_CH2 15
+#define TIMER3_CH3 16
+#define TIMER3_CH4 17
+
+#define TIMER4_CH1 1
+#define TIMER4_CH2 0
+
+// SPI pins
+
+#define SS 7
+#define SCK 6
+#define MISO 5
+#define MOSI 4
+
+typedef struct stm32_pin_info {
+  GPIO_TypeDef *gpio_peripheral;
+  uint8_t gpio_pin;
+};
+
+extern const stm32_pin_info PIN_MAP[];
+
+
+/*
+ * Basic variables
+ */
+
+#define HIGH 0x1
+#define LOW 0x0
+
+/*
+ * GPIO
+ */
+
+typedef enum PinMode {
+  OUTPUT,
+  INPUT,
+  INPUT_PULLUP,
+  INPUT_PULLDOWN
+} PinMode;
+
+void pinMode(uint8_t pin, PinMode mode);
+
+void digitalWrite(uint8_t pin, uint8_t value);
+
+uint32 digitalRead(uint8_t pin);
+
+uint16 analogRead(uint8_t pin);
 
 #endif /* ARDUINITO_H_ */
--- src/arduinito.c
+++ src/arduinito.c
@@ -1,13 +1,135 @@
 /*
- * arduinito.c
+ * Arduinito.c
+ *
+ * Arduinito is a little Arduino implementation.
+ * Designed to run on the STM32, using the ST Standard Peripheral Library.
  *
  *  Created on: Apr 3, 2013
- *      Author: zac
+ *      Author: Zach Supalla
  */
 
 #include "arduinito.h"
 
-void digitalWrite(char pin, char val)
-{
+/*
+ * Globals
+ */
+
+uint8_t adcFirstTime = true;
+
+/*
+ * Pin mapping
+ */
+
+extern const stm2_pin_info PIN_MAP[TOTAL_PINS] = {
+  {GPIOB, 7},
+  {GPIOB, 6},
+  {GPIOB, 5},
+  {GPIOB, 4},
+  {GPIOB, 3},
+  {GPIOA, 15},
+  {GPIOA, 14},
+  {GPIOA, 13},
+  {GPIOA, 8},
+  {GPIOA, 9},
+  {GPIOA, 0},
+  {GPIOA, 1},
+  {GPIOA, 4},
+  {GPIOA, 5},
+  {GPIOA, 6},
+  {GPIOA, 7},
+  {GPIOB, 0},
+  {GPIOB, 1},
+  {GPIOA, 3},
+  {GPIOA, 2},
+  {GPIOA, 10}
+};
+
+/*
+ * Basic variables
+ */
+
+void pinMode(uint8_t pin, PinMode mode) {
+
+  GPIO_TypeDef *gpio = PIN_MAP[pin].gpio_peripheral;
+  uint8_t gpio_pin = PIN_MAP[pin].gpio_pin;
+
+  switch(mode) {
+    case OUTPUT:
+      GPIO_InitTypeDef  GPIO_InitStructure;
+
+      uint32_t RCC_Periph;
+      if (gpio = GPIOA)
+        {RCC_Periph = RCC_APB2Periph_GPIOA}
+      else if (gpio = GPIOB)
+        {RCC_Periph = RCC_APB2Periph_GPIOB}
+      RCC_APB2PeriphClockCmd(RCC_Periph, ENABLE);
+
+      /* Configure the GPIO_LED pin */
+      GPIO_InitStructure.GPIO_Pin = gpio_pin;
+      GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+      GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+      GPIO_Init(gpio, &GPIO_InitStructure);
+      break;
+    case INPUT:
+      break;
+    case INPUT_PULLUP:
+      break;
+    case INPUT_PULLDOWN:
+      break;
+    default:
+      break;
+  }
+}
+
+void adcInit() {
+  ADC_InitTypeDef ADC_InitStructure;
+
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE; 
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = DC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 9;
+
+  ADC_Init(ADC1, &ADC_InitStructure);
+}
+
+void digitalWrite(uint8_t pin, uint8_t value) {
+  if (pin >= TOTAL_PINS) {
+    return;
+  }
+
+  if (value == HIGH) {
+    PIN_MAP[pin].gpio_peripheral>BRR = PIN_MAP[pin].gpio_pin;
+  }
+  else if (value == LOW) {
+    PIN_MAP[pin].gpio_peripheral->BSRR = PIN_MAP[pin].gpio_pin;
+  }
+  
+}
+
+uint32_t digitalRead(uint8_t pin) {
+  if (pin >= TOTAL_PINS) {
+    return;
+  }
+
+  return GPIO_ReadInputDataBit(PIN_MAP[pin].gpio_peripheral, PIN_MAP[pin].gpio_pin);
+}
+
+uint16_t analogRead(uint8_t pin) {
+  pin = pin + FIRST_ANALOG_PIN;
+
+  if (pin >= TOTAL_PINS) {
+    return;
+  }
+
+  if (adcFirstTime == true) {
+    adcInit();
+    adcFirstTime = false;
+  }
+
+
+
 
 }

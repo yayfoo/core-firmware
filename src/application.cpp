0c661245e87153bd9ae713a307e06edc5dc8c65e
yayfoo <jensfougelberg.com+github@gmail.com>
2014-03-04 21:22:19 +0100

    vtdt-v0.4
    Takes long time to even start, but does eventually but fails to parse
    lines.

--- src/application.cpp
+++ src/application.cpp
@@ -6,66 +6,63 @@ void httpRequest(void);
 void count(int number);
 void blink(int onTime, int offTime);
 
-#define BUFF_SIZE 64
-struct time{
+#define BUFF_SIZE 256
+struct time {
     int hours;
     int minutes;
 };
 int led = D7;
 char server[] = "api.vasttrafik.se";
 TCPClient client;
-unsigned long lastConnectionTime = 0;           // last time you connected to the server, in milliseconds
-bool lastConnected = false;                  // state of the connection last time through the main loop
-const unsigned long postingInterval = 10*1000;  // delay between updates, in milliseconds
+unsigned long lastConnectionTime = 0; // last time you connected to the server, in milliseconds
+bool lastConnected = false; // state of the connection last time through the main loop
+const unsigned long postingInterval = 10*1000; // delay between updates, in milliseconds
 bool lastAvailable = false;
-char buff[BUFF_SIZE];
+//char line[BUFF_SIZE];
 struct time servertime={0,0}, realtime={0,0};
 int waittime=0;
 
 void setup() {
    //Initialize serial and wait for port to open:
+   count(2);
    Serial.begin(9600);
    delay(1000);
+   count(5);
 }
 
 void loop() {
-    while (client.available()) {
-        int index=0;
-        char c;
-        int i = 0;
-        do {
-            c = client.read();
-            Serial.write(c);
-            buff[index++] = c;
-            if(i++ > 100) {
-                delay(100);
-                i = 0;
-            }
-        } while (c != '\n');
+uint32_t bytesAvail = client.available();
+uint32_t ptr=0;
+char line[512];
+    while (bytesAvail > 0) {
+        bytesAvail--;
+        line[ptr++] = client.read();
+    }
+line[ptr] = '\0';
 
         Serial.println();
         Serial.println();
-        Serial.print("Created line out of chars:");
+        Serial.print("-> Created line out of chars:");
         Serial.println();
-        Serial.write(buff);
+        Serial.write(line);
         Serial.println();
         Serial.println();
 
         char key1[] = "servertime";
-        char *found = strstr(buff, key1);
+        char *found = strstr(line, key1);
 
         if (found != NULL) {
-            long index = found - buff;
-            servertime.hours = (buff[index+strlen(key1)+3] - '0')*10 + (buff[index+strlen(key1)+4] - '0');
-            servertime.minutes = (buff[index+strlen(key1)+6] - '0')*10 + (buff[index+strlen(key1)+7] - '0');
+            long index = found - line;
+            servertime.hours = (line[index+strlen(key1)+3] - '0')*10 + (line[index+strlen(key1)+4] - '0');
+            servertime.minutes = (line[index+strlen(key1)+6] - '0')*10 + (line[index+strlen(key1)+7] - '0');
         }
 
         char key2[] = "rtTime";
-        found = strstr(buff, key2);
+        found = strstr(line, key2);
         if (found != NULL) {
-            long index = found - buff;
-            realtime.hours = (buff[index+strlen(key2)+3] - '0')*10 + (buff[index+strlen(key2)+4] - '0');
-            realtime.minutes = (buff[index+strlen(key2)+6] - '0')*10 + (buff[index+strlen(key2)+7] - '0');
+            long index = found - line;
+            realtime.hours = (line[index+strlen(key2)+3] - '0')*10 + (line[index+strlen(key2)+4] - '0');
+            realtime.minutes = (line[index+strlen(key2)+6] - '0')*10 + (line[index+strlen(key2)+7] - '0');
 
             //realtime.hours is always equal to or greater than servertime.hours, except at night wrap
             if (realtime.hours < servertime.hours){
@@ -82,7 +79,7 @@ void loop() {
 
             Serial.println();
             Serial.println();
-            Serial.print("Waittime is ");
+            Serial.print("-> Waittime is ");
             Serial.print(waittime);
             Serial.print(" minutes");
             Serial.println();
@@ -92,10 +89,10 @@ void loop() {
             }
         }
         lastAvailable = true;
-    }
+    //}
 
-    if ( client.connected() && lastAvailable && !client.available() ) {
-        Serial.println("stopping client since no more data.");
+    if (client.connected() && lastAvailable && !client.available()) {
+        Serial.println("-> Stopping client since no more data.");
         delay(300);
         client.stop();
         lastAvailable = false;
@@ -105,7 +102,7 @@ void loop() {
     // through the loop, then stop the client:
     if (!client.connected() && lastConnected) {
         Serial.println();
-        Serial.println("disconnecting.");
+        Serial.println("-> Disconnecting.");
         client.stop();
     }
 
@@ -113,7 +110,7 @@ void loop() {
     // your last connection, then connect again and send data:
     if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
         Serial.println();
-        Serial.println("trying to connect again since ten seconds has passed...");
+        Serial.println("-> Trying to connect again since ten seconds has passed...");
         httpRequest();
     }
     // store the state of the connection for next time through
@@ -125,7 +122,7 @@ void loop() {
 void httpRequest() {
     // if there's a successful connection:
     if (client.connect(server, 80)) {
-        Serial.println("connecting...");
+        Serial.println("-> Connecting...");
         // send the HTTP PUT request:
         client.println("GET /bin/rest.exe/v1/departureBoard?authKey=f3c144b3-d974-490e-afc2-ff83d0f7b620&format=json&jsonpCallback=processJSON&id=9021014007900000&direction=9021014001760000&timeSpan=60&maxDeparturesPerLine=1 HTTP/1.1");
         client.println("Host: api.vasttrafik.se");
@@ -137,8 +134,8 @@ void httpRequest() {
         lastConnectionTime = millis();
     } else {
         // if you couldn't make a connection:
-        Serial.println("connection failed");
-        Serial.println("disconnecting.");
+        Serial.println("-> Connection failed!");
+        Serial.println("-> Disconnecting.");
         client.stop();
     }
 }
